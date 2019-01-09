5918967ee2dd9fd5be1f52e5f777c280577e8211
Greg Altman <g.s.altman@gmail.com>
2019-01-09 18:43:29 -0500

    remove cardnum-to-unicode fn cuz it's silly

--- game/cardUtils.js
+++ game/cardUtils.js
@@ -15,20 +15,9 @@ function getRank(cardNum) {
   return cardNum % 10;
 };
 
-function getCardUnicode(cardNum) {
-  const suitChar = [12, 10, 11, 13][getSuit(cardNum)] * 16;
-  const rankChar = [2, 3, 4, 5, 6, 11, 13, 14, 10, 1][getRank(cardNum)];
-
-  return String.fromCodePoint(
-    0xd83c,
-    0xdc00 + suitChar + rankChar
-  );
-}
-
 module.exports = {
   getPoints,
   getPointsForCards,
   getSuit,
-  getRank,
-  getCardUnicode
+  getRank
 }
